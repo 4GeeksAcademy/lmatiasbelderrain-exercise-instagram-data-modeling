b2094e838d4c5e3191e1320c8ad2ed55bf8d0bb0
lmatiasbelderrain <Matibelde@hotmail.com>
2025-02-28 15:16:01 +0000

    modelado

--- src/models.py
+++ src/models.py
@@ -1,31 +1,54 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from sqlalchemy import create_engine, ForeignKey
 from eralchemy2 import render_er
+
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    firtsname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(nullable=False)
+    mail: Mapped[str] = mapped_column(nullable=False)
+    posts=relationship("Post",back_populates="user")
+    comments=relationship("Comment",back_populates="author")
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    # relacion con la tabla Usuario
+    userid: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user=relationship("User",back_populates="posts")
+    # relacion con la tabla Comments
+    comments=relationship("Comment",back_populates="post")
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+class Comments(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(nullable=False)
+    # relacion con la tabla Usuario
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author=relationship("User",back_populates="comments")
+    # relacion con la tabla Post
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post=relationship("Post",back_populates="comments")
+
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
